ca06fbf08673f39c5c5142fd02afc9f57140f490
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-01-25 17:19:43 +0500

    add: create order controller

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,7 @@
 import RazorPay from "razorpay";
 import crypto from "crypto";
+import Transaction from "../models/transaction";
+import Order from "../models/order";
 
 export const createTransaction = async (req, res) => {
   try {
@@ -44,9 +46,63 @@ export const createTransaction = async (req, res) => {
 
 export const createOrder = async (req, res) => {
   try {
-    
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature,
+      userId,
+      cartItems,
+      address,
+      deliveryDate,
+    } = req.body;
+
+    const key_secret = process.env.RAZOR_PAY_KEY_SECRET;
+
+    const generated_signature = crypto
+      .createHmac("sha256", key_secret)
+      .update(`${razorpay_order_id}|${razorpay_payment_id}`)
+      .digest("hex");
+
+    if (generated_signature !== razorpay_signature) {
+      return res.status(400).json({ message: "Payment validation failed" });
+    }
+
+    const transaction = await Transaction.create({
+      user: userId,
+      cartItems,
+      address,
+      deliveryDate,
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature,
+      status: "Success",
+      amount: cartItems.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      ),
+    });
+
+    const order = await Order.create({
+      user: userId,
+      address,
+      deliveryDate,
+      items: cartItems?.map((item) => ({
+        product: item?.productId,
+        quantity: item?.quantity,
+      })),
+      status: "Order Placed",
+    });
+
+    transaction.order = order._id;
+    await transaction.save();
+
+    res
+      .status(200)
+      .json({ message: "Order placed successfully", success: true });
   } catch (error) {
     // RETURN ERROR WITH SUCCESS!
-    return res.status(500).json({ message: error.message });
+    return res
+      .status(500)
+      .json({ message: error.message, error: true, status: "Failed" });
   }
 };
