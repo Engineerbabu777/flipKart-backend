25f05fd1299893eef4cd607e6101b48367628390
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-01-26 13:04:20 +0500

    add:/ adminjs setup added

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import categoryRoutes from "./routes/category.js";
 import productRoutes from "./routes/product.js";
 import orderRoutes from "./routes/order.js";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import connectDB from "./config/connect.js";
+import { buildAdminJS } from "./config/setup.js";
 
 const app = express();
 dotenv.config();
@@ -16,12 +18,14 @@ app.use("/category", categoryRoutes);
 app.use("/product", productRoutes);
 app.use("/order", orderRoutes);
 
-
-
 const start = async () => {
   // RUN SERVER!
   try {
-    app.listen({ port: 3000, host: "0.0.0.0" }, () => {
+    await connectDB(process.env.MONGO_URI);
+
+    await buildAdminJS(app);
+
+    app.listen({ port: 3000}, () => {
       console.log("Server is running on port 3000");
     });
   } catch (error) {
